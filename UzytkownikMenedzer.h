1a794a43a151fa8e877870003c567fc9353812c8
Michal Powalek <michal.powalek.programista@gmail.com>
2023-09-08 17:34:05 +0200

    Divide KsiazkaAdresowa to smaller class

---
+++ UzytkownikMenedzer.h
@@ -0,0 +1,33 @@
+#ifndef UZYTKOWNIKMENEDZER_H
+#define UZYTKOWNIKMENEDZER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+#include "PlikZUzytkownikami.h"
+#include "MetodyPomocnicze.h"
+
+
+using namespace std;
+
+class UzytkownikMenedzer
+{
+    int idZalogowanegoUzytkownika;
+    vector <Uzytkownik> uzytkownicy;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+    PlikZUzytkownikami plikZUzytkownikami;
+
+public:
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+
+};
+
+#endif
